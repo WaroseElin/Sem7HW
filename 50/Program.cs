86e6130a64632567fc2deda43bd5b0e543553737
Andrew <sawosinandreww@mail.ru>
2023-04-17 10:07:49 +0300

    Дз

---
+++ 50/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("введите номер строки");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int b = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (a > numbers.GetLength(0) || b > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {a} строки и {b} столбца равно {numbers[a-1,b-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        
+        Console.WriteLine(""); 
+    }
+}
